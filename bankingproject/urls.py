7a60de0fab4a70e852b19fd2402b6da5ad9ca314
Rasika35 <85476341+Rasika35@users.noreply.github.com>
2021-06-20 22:46:20 +0530

    Update urls.py

--- bankingproject/urls.py
+++ bankingproject/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bankingapp.urls')),
 
-   
+    url(r'^media/(?P<path>,*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^STATIC/(?P<path>,*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
