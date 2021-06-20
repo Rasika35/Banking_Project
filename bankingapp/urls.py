f5f587c1971b4efed99312c9fc86f78f64eb176a
Rasika35 <85476341+Rasika35@users.noreply.github.com>
2021-06-20 22:27:25 +0530

    Update urls.py

--- bankingapp/urls.py
+++ bankingapp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from bankingapp import views
 
 urlpatterns = [
-    path('', views.Homepage.as_view(), name="homepage"),
+    path('index/', views.Homepage.as_view(), name="homepage"),
     path('view_all_customers/', views.ViewAllCustomers.as_view(), name="view_all_customers"),
     path('customer_detail/<pk>/', views.CustomerDetail.as_view(), name="customer_detail"),
     path('transfer_money/<pk>/', views.TransferMoney.as_view(), name="transfer_money"),
